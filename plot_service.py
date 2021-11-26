776bb857531c7a929cf361e780b416a7ce415c75
Martin Miglio <marmig0404@gmail.com>
2021-11-26 01:04:56 -0500

    plot fixes and updates updated plotly configuration changed db parsing

--- plot_service.py
+++ plot_service.py
@@ -1,10 +1,12 @@
 import plotly.express as px
+import re
 import sqlite3
 
 con = sqlite3.connect('locations.db')
 cur = con.cursor()
 
 execution_string = "SELECT * FROM dev"
+print("Generating new plot from query: \n" + execution_string)
 epochs = []
 datetimes = []
 lats = []
@@ -12,12 +14,18 @@ longs = []
 for epoch, datetime, lat, long in cur.execute(execution_string):
     epochs.append(epoch)
     datetimes.append(datetime)
-    lat_number = float(lat[-len(lat)])
-    lats.append(float(lat[-len(lat)])
-                if "N" in lat else -1*float(lat[-len(lat)]))
-    longs.append(float(long[-len(long)])
-                 if "E" in long else -1*float(long[-len(long)]))
-
+    digit_lat = float(re.sub('\D', '', lat))
+    parsed_lat = float(digit_lat if "N" in lat else -1*digit_lat)
+    lats.append(parsed_lat)
+    digit_long = float(re.sub('\D', '', long))
+    parsed_long = digit_long if "E" in long else -1*digit_long
+    longs.append(parsed_long)
 fig = px.scatter(x=longs, y=lats, color=epochs,
-                 label=datetimes, template="simple_white")
+                 hover_name=datetimes, template="simple_white", title='GPS Data', labels={'x': '', 'y': ''})
+fig.update_layout(coloraxis_showscale=False)
+fig.update_yaxes(
+    scaleanchor="x",
+    scaleratio=1,
+)
+print("Wrote plot to plot.html")
 fig.write_html("plot.html")
